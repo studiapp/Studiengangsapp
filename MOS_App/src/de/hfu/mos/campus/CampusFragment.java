fa6cf99ba9c292778c7b2d05a79ad9ed4d0eff87
Siggi <fastsieg@hs-furtwangen.de>
2015-01-16 19:06:50 +0100

    - Campus crashfix

--- MOS_App/src/de/hfu/mos/campus/CampusFragment.java
+++ MOS_App/src/de/hfu/mos/campus/CampusFragment.java
@@ -17,7 +17,24 @@ public class CampusFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		View rootView = inflater.inflate(R.layout.fragment_campus, container, false);
+	    
+		resetFragments();
+		
 		return rootView;
 	}
-
+	
+	// fragments have to be reset or app will crash if they are called a second time and still referenced
+	private void resetFragments(){
+		
+	    Fragment f = (Fragment) getFragmentManager().findFragmentById(R.id.campus_buttonFragment);
+	    if (f != null){ 
+	        getFragmentManager().beginTransaction().remove(f).commit();
+	    }
+	    
+	    f = (Fragment) getFragmentManager().findFragmentById(R.id.campus_mapFragment);
+	    if (f != null){ 
+	        getFragmentManager().beginTransaction().remove(f).commit();
+	    }
+	}
+	
 }
